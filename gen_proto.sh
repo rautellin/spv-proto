3d7afe95c347dbd9e124d673589e37f76eebf3b1
rautellinn <nathalie@rautellinn.com>
2024-11-22 09:23:22 +0100

    chore: added buf lint and restructured proto files according to linter

--- gen_proto.sh
+++ gen_proto.sh
@@ -5,23 +5,38 @@ set -e
 if [ -z "$SPV_PROTO_PATH" ]; then
     echo "SPV_PROTO_PATH is not set. Falling back to current directory."
     SPV_PROTO_PATH=$(pwd)
-else
-    SPV_PROTO_PATH=$SPV_PROTO_PATH
 fi
 
-echo "Generating protobuf files..."
+echo "Current directory: $SPV_PROTO_PATH"
+echo "Starting Buf linting..."
 
-# Set the output directory for generated Go code
+# Run Buf linting
+buf lint "$SPV_PROTO_PATH/protobuf"
+if [ $? -ne 0 ]; then
+    echo "Buf linting failed. Please fix the linting errors."
+    exit 1
+fi
+
+echo "Buf linting passed. Proceeding to generate protobuf files..."
+
+# Set output directories for Go and Dart code
 go_output_dir="$SPV_PROTO_PATH/gen/go"
-# Set the output directory for generated Dart code
 dart_output_dir="$SPV_PROTO_PATH/gen/dart/lib"
 
-cd protobuf
+# Find all .proto files recursively
+proto_files=$(find "$SPV_PROTO_PATH/protobuf" -name "*.proto")
 
 # Generate Go code
-protoc --proto_path=$SPV_PROTO_PATH/protobuf --go_out="$go_output_dir" --go_opt=paths=source_relative --go-grpc_out="$go_output_dir" --go-grpc_opt=paths=source_relative *.proto
-# Generate Dart code
-protoc -Iprotobuf --proto_path=$SPV_PROTO_PATH/protobuf --dart_out=grpc:"$dart_output_dir" *.proto google/protobuf/timestamp.proto
-cd ..
+protoc -I"$SPV_PROTO_PATH/protobuf" \
+       --go_out="$go_output_dir" --go_opt=paths=source_relative \
+       --go-grpc_out="$go_output_dir" --go-grpc_opt=paths=source_relative \
+       $proto_files
 
-echo "Protobuf generation completed."
+# Generate Dart code
+protoc -I"$SPV_PROTO_PATH/protobuf" \
+       --dart_out=grpc:"$dart_output_dir" \
+       $proto_files \
+       google/protobuf/timestamp.proto \
+       google/protobuf/field_mask.proto
+
+echo "Protobuf generation completed successfully."
